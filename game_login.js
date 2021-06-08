4aeeae846f2a51bcce224a9b247adddd24ad09cc
Harishsai123 <77489996+Harishsai123@users.noreply.github.com>
2021-06-08 14:03:38 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,14 @@
+
+
+function addUser() {
+ var player1_name;
+ var player2_name;
+ 
+ player1_name = document.getElementById("player_1_name").value;
+ player2_name = document.getElementById("player_2_name").value;
+
+ localStorage.setItem("player_1_name", player1_name);
+ localStorage.setItem("player_2_name", player2_name);
+
+ window.location = "game_page.html";
+}
